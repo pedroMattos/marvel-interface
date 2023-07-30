b3bf0d2becc615dc37551a56cab2b0eae06239a1
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-30 19:21:58 -0300

    fix identation

--- src/components/Buttons/FormSubmitButton/index.js
+++ src/components/Buttons/FormSubmitButton/index.js
@@ -4,8 +4,8 @@ import { CustomButton } from "./styles";
 export default function FormSubmitButton({ text, onClick, isDisabled = false, hasIcon = false }) {
   return (
     <CustomButton variant="contained"
-    disabled={isDisabled}
-    onClick={onClick}
-    endIcon={hasIcon && <LoginIcon width={13} height={13} />}>{text}</CustomButton>
+      disabled={isDisabled}
+      onClick={onClick}
+      endIcon={hasIcon && <LoginIcon width={13} height={13} />}>{text}</CustomButton>
   )
 }
