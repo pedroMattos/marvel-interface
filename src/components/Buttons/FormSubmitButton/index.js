bcb3c0d984ebf20b8a1f95d7138d54ab1c8f54a6
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-30 13:50:06 -0300

    add more props on subtmi button

--- src/components/Buttons/FormSubmitButton/index.js
+++ src/components/Buttons/FormSubmitButton/index.js
@@ -1,6 +1,10 @@
 import LoginIcon from "../../Icons/LoginIcon";
 import { CustomButton } from "./styles";
 
-export default function FormSubmitButton() {
-  return (<CustomButton variant="contained" endIcon={<LoginIcon width={13} height={13} />}>entrar</CustomButton>)
+export default function FormSubmitButton({ text, isDisabled = false, hasIcon = false }) {
+  return (
+    <CustomButton variant="contained"
+    disabled={isDisabled}
+    endIcon={hasIcon && <LoginIcon width={13} height={13} />}>{text}</CustomButton>
+  )
 }
