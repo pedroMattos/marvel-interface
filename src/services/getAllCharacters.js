038fe5362274df068bb2f9f8347c5c34c9efff0d
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-30 19:23:17 -0300

    fix get all characters service

--- src/services/getAllCharacters.js
+++ src/services/getAllCharacters.js
@@ -6,10 +6,10 @@ const privateKey = '80268368a463e73b04fa43dc2acbbc2e026f4ba0'
 async function getAllCharacters() {
   const timestamp = new Date().getTime()
   const md5Hash = md5(timestamp + privateKey + publicKey)
-  const characters = await axios
-    .get(`http://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${publicKey}&hash=${md5Hash}`)
+  const { data } = await axios
+    .get(`http://gateway.marvel.com/v1/public/characters?limit=20&ts=${timestamp}&apikey=${publicKey}&hash=${md5Hash}`)
 
-  console.log({characters})
+  return data.data.results
 }
 
 export default getAllCharacters
