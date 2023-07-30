4eb55fef245b6ac8ddc3f9df013923c63a7c51dc
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-30 19:22:10 -0300

    add home page enter button

---
+++ src/components/Buttons/HomePageEnter/index.js
@@ -0,0 +1,7 @@
+import { CustomButton } from "./styles";
+
+export default function HomePageEnter({ onClick }) {
+  return (
+    <CustomButton variant="contained" onClick={onClick}>Entrar</CustomButton>
+  )
+}
