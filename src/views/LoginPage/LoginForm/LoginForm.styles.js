b7034a8dd9c2b738eab39f64307c53bdc0bf536c
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-30 13:07:12 -0300

    add actions area

--- src/views/LoginPage/LoginForm/LoginForm.styles.js
+++ src/views/LoginPage/LoginForm/LoginForm.styles.js
@@ -1,1 +1,9 @@
 import { styled } from "styled-components";
+
+export const Actions = styled.div`
+  display: flex;
+  width: 310px;
+  margin-top: 23px;
+  justify-content: flex-end;
+  padding: 0 36px;
+`
