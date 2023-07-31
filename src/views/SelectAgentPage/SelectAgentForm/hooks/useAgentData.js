9d28b3129ea4e80438e95ee5f73eefc62d67cac9
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-31 16:12:14 -0300

    add pagination logic

--- src/views/SelectAgentPage/SelectAgentForm/hooks/useAgentData.js
+++ src/views/SelectAgentPage/SelectAgentForm/hooks/useAgentData.js
@@ -1,13 +1,29 @@
 import { useEffect, useState } from "react";
 import getAllCharacters from "../../../../services/getAllCharacters";
-
+const PER_PAGE = 10
 export default function useAgentData() {
   const [agents, setAgents] = useState(null)
+  const [totalItems, setTotalItems] = useState(0)
   const [isLoading, setIsLoading] = useState(true)
 
+  const handlePageChange = (event, page) => {
+    console.log(event.target.textContent, page)
+    const offset = (page - 1) * PER_PAGE
+    reFetchAgents(offset)
+    setIsLoading(true)
+  }
+
+  const reFetchAgents = async (offset) => {
+    const { results, total } = await getAllCharacters(offset)
+    setAgents(results)
+    setTotalItems(total)
+    setIsLoading(false)
+  }
+
   const fetchAgents = async () => {
-    const fetchedAgents = await getAllCharacters()
-    setAgents(fetchedAgents)
+    const { results, total } = await getAllCharacters()
+    setAgents(results)
+    setTotalItems(total)
     setIsLoading(false)
   }
 
@@ -15,5 +31,5 @@ export default function useAgentData() {
     fetchAgents()
   }, [])
 
-  return { isLoading, agents }
+  return { isLoading, agents, totalItems, handlePageChange }
 }
