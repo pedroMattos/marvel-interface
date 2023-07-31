8a4f472ec745aedd79fd4a295a99329ae75979cc
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-31 16:11:46 -0300

    add offset support to endpoint

--- src/services/getAllCharacters.js
+++ src/services/getAllCharacters.js
@@ -3,13 +3,13 @@ import md5 from 'md5'
 const publicKey = '8a2e07fa56ea6c6640fcc0127602b791'
 const privateKey = '80268368a463e73b04fa43dc2acbbc2e026f4ba0'
 
-async function getAllCharacters() {
+async function getAllCharacters(offset = 0) {
   const timestamp = new Date().getTime()
   const md5Hash = md5(timestamp + privateKey + publicKey)
   const { data } = await axios
-    .get(`http://gateway.marvel.com/v1/public/characters?limit=10&ts=${timestamp}&apikey=${publicKey}&hash=${md5Hash}`)
+    .get(`http://gateway.marvel.com/v1/public/characters?limit=10&offset=${offset}&ts=${timestamp}&apikey=${publicKey}&hash=${md5Hash}`)
 
-  return data.data.results
+  return data.data
 }
 
 export default getAllCharacters
