46cdd8e772218747c02bf7b0ed9dae05a48323f1
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-31 18:33:57 -0300

    add start with on endpoint

--- src/services/getAllCharacters.js
+++ src/services/getAllCharacters.js
@@ -3,11 +3,13 @@ import md5 from 'md5'
 const publicKey = '8a2e07fa56ea6c6640fcc0127602b791'
 const privateKey = '80268368a463e73b04fa43dc2acbbc2e026f4ba0'
 
-async function getAllCharacters(offset = 0) {
+async function getAllCharacters(offset = 0, searchTerm = '') {
+  const startsWithSearch = `nameStartsWith=${searchTerm}&`
+  const hasSearch = !!searchTerm.trim()
   const timestamp = new Date().getTime()
   const md5Hash = md5(timestamp + privateKey + publicKey)
   const { data } = await axios
-    .get(`http://gateway.marvel.com/v1/public/characters?limit=10&offset=${offset}&ts=${timestamp}&apikey=${publicKey}&hash=${md5Hash}`)
+    .get(`http://gateway.marvel.com/v1/public/characters?limit=10&${hasSearch ? startsWithSearch : ''}offset=${offset}&ts=${timestamp}&apikey=${publicKey}&hash=${md5Hash}`)
 
   return data.data
 }
