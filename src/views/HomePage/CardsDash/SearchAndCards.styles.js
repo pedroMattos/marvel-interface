3c9371d2655ec7650438710aac45c6c9bc7feaff
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-31 00:41:45 -0300

    add search and cards styles

---
+++ src/views/HomePage/CardsDash/SearchAndCards.styles.js
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+
+export const CardsAreaWrapper = styled.div`
+  padding: 20px 27px;
+  display: flex;
+  gap: 10px;
+  flex-wrap: wrap;
+  justify-content: center;
+`
