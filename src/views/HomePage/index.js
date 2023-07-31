6a04d05a720800bcff341e5dfa2127d793823f35
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-31 00:41:26 -0300

    import section and info

--- src/views/HomePage/index.js
+++ src/views/HomePage/index.js
@@ -1,9 +1,12 @@
 import Sidebar from "../Sidebar";
+import { PageSection } from "../views.styles";
+import SearchAndCards from "./CardsDash/SearchAndCards";
 
 export default function HomePage() {
   return (
-    <>
+    <PageSection>
       <Sidebar />
-    </>
+      <SearchAndCards />
+    </PageSection>
   )
 }
