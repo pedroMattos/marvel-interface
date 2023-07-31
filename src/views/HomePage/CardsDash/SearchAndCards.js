7c6c4516db4f4849a2319862ab222c66b473654f
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-31 16:11:56 -0300

    add pagination

--- src/views/HomePage/CardsDash/SearchAndCards.js
+++ src/views/HomePage/CardsDash/SearchAndCards.js
@@ -3,16 +3,23 @@ import AgentCard from "../../../components/Cards/AgentCard";
 import useAgentData from "../../SelectAgentPage/SelectAgentForm/hooks/useAgentData";
 import { PageInfo } from "../../views.styles";
 import SearchBar from "../SearchBar";
-import { CardsAreaWrapper } from "./SearchAndCards.styles";
+import { CardsAreaWrapper, CustomDivider, PaginatorArea } from "./SearchAndCards.styles";
+import PaginationSelector from "../../../components/PaginationSelector";
 
 export default function SearchAndCards() {
-  const { agents, isLoading } = useAgentData()
+  const { agents, isLoading, totalItems, handlePageChange } = useAgentData()
+  const totalItemsPerPage = 10
+  const totalPages = parseInt(totalItems / totalItemsPerPage)
   return (
     <PageInfo>
       <SearchBar />
       <CardsAreaWrapper>
         {!isLoading ? agents.map((agent) => (<AgentCard agentData={agent} />)) : <CircularProgress />}
       </CardsAreaWrapper>
+      <PaginatorArea>
+        <CustomDivider />
+        <PaginationSelector count={totalPages} onChange={handlePageChange} />
+      </PaginatorArea>
     </PageInfo>
   )
 }
