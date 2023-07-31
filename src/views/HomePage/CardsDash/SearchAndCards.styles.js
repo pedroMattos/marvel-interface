c3a891c39a395a90c77f2953ca4b0b6774da97fe
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-31 16:12:07 -0300

    paginator area styles

--- src/views/HomePage/CardsDash/SearchAndCards.styles.js
+++ src/views/HomePage/CardsDash/SearchAndCards.styles.js
@@ -1,3 +1,4 @@
+import { Divider } from "@mui/material";
 import { styled } from "styled-components";
 
 export const CardsAreaWrapper = styled.div`
@@ -6,4 +7,17 @@ export const CardsAreaWrapper = styled.div`
   gap: 10px;
   flex-wrap: wrap;
   justify-content: center;
+  align-items: center;
+  min-height: 470px;
+`
+
+export const PaginatorArea = styled.div`
+  margin-top: 12px;
+`
+
+export const CustomDivider = styled(Divider)`
+&.MuiDivider-root {
+  margin-bottom: 12px;
+  border-color: var(--divider-color);
+}
 `
