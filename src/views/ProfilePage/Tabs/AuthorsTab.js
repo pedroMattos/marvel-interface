5720f61426b68c683a9c92701e97d3f53b5cc2df
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-08-04 14:55:52 -0300

    add tabs containers

---
+++ src/views/ProfilePage/Tabs/AuthorsTab.js
@@ -0,0 +1,18 @@
+import { Box, Typography } from "@mui/material";
+
+export default function AuthorsTab({ children, value, index }) {
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  )
+}
