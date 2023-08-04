990ec40262d8e267f62b82781fda4a5f60505c7b
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-08-04 14:55:26 -0300

    add mapstate store

--- src/views/ProfilePage/index.js
+++ src/views/ProfilePage/index.js
@@ -1,13 +1,24 @@
 import { useParams } from "react-router-dom"
 import { PageSection } from "../views.styles"
 import Sidebar from "../Sidebar"
+import TabWrapper from "./TabWrapper"
+import { connect } from "react-redux"
 
-export default function ProfilePage() {
+const mapStateToProps = (state) => {
+  return {
+    data: state.data.data,
+  };
+}
+
+function ProfilePage({ data }) {
   const { id } = useParams()
 
   return (
     <PageSection>
       <Sidebar />
+      <TabWrapper agentData={data} />
     </PageSection>
   )
 }
+
+export default connect(mapStateToProps)(ProfilePage)
