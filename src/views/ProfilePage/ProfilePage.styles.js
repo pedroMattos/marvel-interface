7cfb232fa53b5233dc3592eb903d100ec3f13221
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-08-04 14:55:38 -0300

    add profile page styles

---
+++ src/views/ProfilePage/ProfilePage.styles.js
@@ -0,0 +1,31 @@
+import { styled } from "styled-components";
+import { FormTitle } from "../forms.styles";
+
+export const PageTitle = styled(FormTitle)`
+  padding: 0;
+  margin-bottom: 0;
+`
+
+export const TabContainer = styled.div`
+  padding: 20px 27px;
+`
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`
+
+export const AgentName = styled.p`
+  margin: 0;
+  font-size: 24px;
+  color: var(--gray-500);
+  letter-spacing: -0.72px;
+  font-weight: 300;
+`
+
+export const Emptyheader = styled.div`
+  height: 59px;
+  width: 100%;
+  border: 1px solid var(--divider-color);
+`
