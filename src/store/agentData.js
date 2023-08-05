77c36d2f1aba53d0d3545bd113638b63543f463f
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-08-04 23:00:53 -0300

    add reducer

--- src/store/agentData.js
+++ src/store/agentData.js
@@ -1,11 +1,14 @@
 const initialState = {
   data: null,
+  defaultAgent: null
 }
 
 const dataReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'UPDATE_DATA':
       return { ...state, data: action.payload }
+    case 'CREATE_DEFAULT_AGENT':
+      return { ...state, defaultAgent: action.payload }
     default:
       return state
   }
