b4c9b0bf56509c6d4f98969dffa1f20fca9824ed
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-08-05 00:57:13 -0300

    loading styles

---
+++ src/components/Loading/styles.js
@@ -0,0 +1,10 @@
+import { styled } from "styled-components";
+
+export const LoadingContainer = styled.div`
+  width: 100px;
+  height: 100px;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
