d43572b3413a19039326288e31c3c4aac3084733
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-08-04 22:43:41 -0300

    add navigation

--- src/components/Buttons/HelpRecover/index.js
+++ src/components/Buttons/HelpRecover/index.js
@@ -1,6 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import HelpShieldIcon from "../../Icons/HelpShieldIcon";
 import { CustomButtom } from "./styles";
 
 export default function HelpRecover() {
-  return <CustomButtom size="small" startIcon={<HelpShieldIcon width={14} height={14} />}>Esqueceu a senha?</CustomButtom>
+  const navigate = useNavigate()
+
+  const handleHelpRecover = () => navigate('/recover-account')
+  return <CustomButtom onClick={handleHelpRecover} size="small" startIcon={<HelpShieldIcon width={14} height={14} />}>Esqueceu a senha?</CustomButtom>
 }
