6ea9852623fcd67b6733ae963c2b38620546d8c2
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-08-04 23:01:08 -0300

    add navigation params

--- src/views/SelectAgentPage/SelectAgentForm/SelectAgentForm.js
+++ src/views/SelectAgentPage/SelectAgentForm/SelectAgentForm.js
@@ -1,18 +1,30 @@
+import { connect } from "react-redux";
 import HomePageEnter from "../../../components/Buttons/HomePageEnter";
 import SelectWithThumb from "../../../components/Selects/SelectWithThumb";
 import { FormContainer, FormTitle, InfoTextForm } from "../../forms.styles";
 import useAgentData from "./hooks/useAgentData";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function SelectAgentForm() {
+function SelectAgentForm({ dispatch }) {
+  const navigate = useNavigate()
   const { agents, isLoading } = useAgentData()
+  const [selectedAgent, setSelectedAgent] = useState(null)
+
+  const handleSelectAgent = () => {
+    navigate('/')
+    dispatch({ type: 'CREATE_DEFAULT_AGENT', payload: selectedAgent })
+  }
   return (
     <FormContainer>
     <FormTitle $highLightText=".">Selecione o seu agente mais legal</FormTitle>
     <InfoTextForm>
       Tenha a visão do seu agente
     </InfoTextForm>
-    {!isLoading && <SelectWithThumb listData={agents} />}
-    <HomePageEnter />
+    {!isLoading && <SelectWithThumb setSelectedAgent={setSelectedAgent} selectedAgent={selectedAgent} listData={agents} />}
+    <HomePageEnter onClick={handleSelectAgent} />
   </FormContainer>
   )
 }
+
+export default connect()(SelectAgentForm)
