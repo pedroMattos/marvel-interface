88dde05bad48dfdd7f7c16dcbb2a135f788c3bce
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-08-05 00:57:04 -0300

    add loading component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,6 @@
+import { CircularProgress } from "@mui/material";
+import { LoadingContainer } from "./styles";
+
+export default function Loading() {
+  return(<LoadingContainer><CircularProgress /></LoadingContainer>)
+}
